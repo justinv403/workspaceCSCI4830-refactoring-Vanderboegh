9b2000b8e75ba1e1e3f7080a379d88ef70bd628a
Justin <justinv403@gmail.com>
2023-10-07 20:43:19 -0500

    Extracted method for q1

--- simple-refactoring-examples/src/q1/extract_method/refactored/A.java
+++ simple-refactoring-examples/src/q1/extract_method/refactored/A.java
@@ -6,12 +6,16 @@ public class A {
    Node m1(List<Node> nodes, String p) {
       // TODO: Your answer
       // other implementation
+	  printElements(nodes, p);
+	   
       return null;
    }
 
    Edge m2(List<Edge> edgeList, String p) {
       // TODO: Your answer
       // other implementation
+	  printElements(edgeList, p);
+	   
       return null;
    }
 
@@ -19,6 +23,28 @@ public class A {
    // ? extractedMethod(?) {
    //
    // }
+   // printElements method using extract method technique
+   void printElements(List<?> elements, String p) {
+	   // loop over elements in the provided list
+	   for (Object element : elements) {
+		   
+		   // print nodes if that is the current type
+		   if (element instanceof Node) {
+			   // set current node and print
+			   Node node = (Node) element;
+			   if(node.contains(p)) {
+				   System.out.println(node);
+			   }
+		   // print edges if that is the current time
+		   } else if (element instanceof Edge) {
+			   // set current edge and print
+			   Edge edge = (Edge) element;
+			   if(edge.contains(p)) {
+				   System.out.println(edge);
+			   }
+		   }
+	   }
+   }
 }
 
 class Node {
